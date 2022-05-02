be5c70405e1563ff15f79508630c75cec0d5228b
billvivino <billvivino@gmail.com>
2022-05-02 19:34:22 -0400

    Initial Commit

---
+++ Images/ImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesApp.swift
+//  Images
+//
+//  Created by Bill Vivino on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
